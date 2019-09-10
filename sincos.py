bc0eba284eec799526fbb3446e2b414adf6c7d29
Shujaat Bakhsh <shujaatbakhsh25@gmail.com>
2019-09-10 19:32:07 -0400

    Added tan function

--- sincos.py
+++ sincos.py
@@ -8,14 +8,11 @@ Created on Tue Sep 10 15:55:08 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-x = np.arange(0,4*np.pi,0.1)
+x = np.arange(0, 4 * np.pi, 0.1)
 y = np.sin(x)
 z = np.cos(x)
+w = np.tan(x)
 
-plt.plot(x,y,x,z)
-plt.xlabel('Values from 0 to 4pi')
-plt.ylabel('sin(x) and cos(x)')
-plt.title('sin and cos function for one period')
-plt.legend('sin(x)', 'cos(x)')
+plt.plot(x, y, x, z, x, w)
+
 plt.show()
